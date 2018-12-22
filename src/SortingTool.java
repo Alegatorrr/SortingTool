0d5238fea5f1181d85eeebb85aa7a16e0c891ed7
Alegatorrr <alegatorrr@bk.ru>
2018-12-22 22:58:26 +0300

    Stage #1: The greatest number

---
+++ src/SortingTool.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class SortingTool {
+    public static void main(final String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<Long> myList = new ArrayList<>();
+
+        while (scanner.hasNextLong()) {
+            final long number = scanner.nextLong();
+            myList.add(number);}
+
+        Collections.sort(myList);
+
+        long X = myList.size();
+        long Y = myList.get(myList.size()-1);
+
+        System.out.println("Total numbers: " + X +".");
+
+        System.out.println("The greatest number: " + Y + " (1 time(s))"+".");
+
+    }
+}
